74e03e2ed8b82bcf8c06fcfa30803fbb56caa33c
aattanasio91 <agustin.attanasio@gmail.com>
2021-09-26 17:19:27 -0300

    Commit inicial

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const formulario = document.getElementById('formulario');
+
+document.getElementById('formulario').addEventListener('submit', function() {
+    const dataUser = document.getElementById('dataUser');
+    const dataPass = document.getElementById('dataPass');
+    if (validarData(dataUser, dataPass)) {
+        location.href = "homePage.html";
+    } else {
+        alert('Usuario o password incorrecto. Recuerda que el usuario deben ser unicamente numeros y la contrasena requiere como minimo, 4 caracteres');
+    }
+});
+
+function validarData(dataUser, dataPass) {
+    var esValido = false;
+    if (validarUser(dataUser) && validarPass(dataPass)) {
+        esValido = true;
+    }
+    return esValido;
+}
+
+function validarUser(dataUser) {
+    var result = false;
+    if (!tiene_letras(dataUser.value) && tiene_solo_numeros(dataUser.value)) {
+        result = true;
+    }
+    return result;
+}
+
+function validarPass(dataPass) {
+    var result = false;
+    if (dataPass.value.length >= 4) {
+        result = true;
+    }
+    return result;
+}
+
+function tiene_solo_numeros(texto) {
+    var numeros = "0123456789";
+    var resultado = 0;
+    for (i = 0; i < texto.length; i++) {
+        if (numeros.indexOf(texto.charAt(i), 0) != -1) {
+            resultado = 1;
+        } else {
+            resultado = 0;
+        }
+    }
+    return resultado;
+}
+
+function tiene_letras(texto) {
+    var letras = "abcdefghyjklmnñopqrstuvwxyz";
+    texto = texto.toLowerCase();
+    for (i = 0; i < texto.length; i++) {
+        if (letras.indexOf(texto.charAt(i), 0) != -1) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+
+document.getElementById('logout').addEventListener('click', () => {
+    location.href = "index.html";
+});
